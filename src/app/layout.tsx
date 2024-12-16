4f78d2855123ba1f6d82bc846d47bf3e2a45886c
ali saghir <alisaghir543@gmail.com>
2024-12-16 16:20:04 +0400

    added a script for ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -15,7 +16,8 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "فلل دبي الفاخرة | شراء، تأجير، واستثمار في العقارات",
-  description:'اكتشف الفيلا المثالية مع Hi5 Villas، شريكك الموثوق في العقارات. استكشف مجموعة واسعة من الفلل الفاخرة المصممة لتناسب أسلوب حياتك. سواء كنت تشتري، تستأجر، أو تبيع، نحن نجعل رحلتك مع الفلل سهلة وخالية من الإجهاد',
+  description:
+    "اكتشف الفيلا المثالية مع Hi5 Villas، شريكك الموثوق في العقارات. استكشف مجموعة واسعة من الفلل الفاخرة المصممة لتناسب أسلوب حياتك. سواء كنت تشتري، تستأجر، أو تبيع، نحن نجعل رحلتك مع الفلل سهلة وخالية من الإجهاد",
   icons: {
     icon: "/logo.svg", // Replace with the path to your logo image in the public folder
   },
@@ -28,6 +30,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-YEXE5ZFSCY"
+          async
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-YEXE5ZFSCY');
+  `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
